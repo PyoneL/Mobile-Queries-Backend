2fb45cdeba1ea2bc70d486cc58f359d31800dc59
Engin Yenice <enginyenice2626@gmail.com>
2021-04-20 04:31:23 +0300

    FirebaseOperationTypeTwoDal -> ArticleTwo fixed.

--- DataAccess/Concrete/Firebase/FirebaseOperationTypeTwoDal.cs
+++ DataAccess/Concrete/Firebase/FirebaseOperationTypeTwoDal.cs
@@ -63,7 +63,7 @@ namespace DataAccess.Concrete.Firebase
                             }
                             ).OrderBy(p => p.total_amount_average).ToList();
 
-            var response2 = response.Take(2).ToList();
+            var response2 = response.Take(2).OrderBy(p => p.PUDatetime).ToList();
 
             var response3 = (from a in response
                              where a.PUDatetime.Date >= response2[0].PUDatetime.Date &&
